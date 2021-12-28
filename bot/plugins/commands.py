8f5c62b720528308b177d5d32e735772ffd8cefc
mdjdjieururu <96656667+mdjdjieururu@users.noreply.github.com>
2021-12-28 21:40:18 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,7 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name} /n https://t.me/mallumoviegrup"‚
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
