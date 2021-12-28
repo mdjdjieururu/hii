7dbafcebffa41c485d15b7a76adcf429699f71c8
mdjdjieururu <96656667+mdjdjieururu@users.noreply.github.com>
2021-12-28 22:15:40 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -206,7 +206,7 @@ async def auto_filter(bot: Client, update: Message):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"https://t.me/mallmovieup {(len_results)} Results For Your Request: <code>{query}</code>",
+                text=f"https://t.me/mallmovieup {(len_results)} Results For Your Request 🌟: <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
