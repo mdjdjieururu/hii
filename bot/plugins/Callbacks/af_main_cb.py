f45b355be40cf82865b4144e57e3851f74eaa6a5
mdjdjieururu <96656667+mdjdjieururu@users.noreply.github.com>
2021-12-28 22:02:16 +0530

    Update af_main_cb.py

--- bot/plugins/Callbacks/af_main_cb.py
+++ bot/plugins/Callbacks/af_main_cb.py
@@ -92,7 +92,7 @@ async def cb_navg(bot, update: CallbackQuery):
 
     if not int(index_val) == 0:    
         temp_results.append([
-            InlineKeyboardButton(f"🔰 Page {index_val + 1}/{len(results) if len(results) < max_pages else max_pages} 🔰", callback_data="ignore")
+            InlineKeyboardButton(f"⭕ Page {index_val + 1}/{len(results) if len(results) < max_pages else max_pages} ⭕", callback_data="ignore")
         ])
     
     if show_invite and int(index_val) !=0 :
@@ -139,7 +139,7 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
     
-    text=f"<i>Found</i> <code>{leng}</code> <i>Results For Your Query:</i> <code>{query}</code>"
+    text=f"<i>https://t.me/mallmovieup</i> <code>{leng}</code> <i>Results For Your Request 🌟:</i> <code>{query}</code>"
         
     try:
         await update.message.edit(
